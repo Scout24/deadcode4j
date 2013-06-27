5b23cddecc965f212da3eeba92bb214ffa01cfe4
sebastiankirsch <Sebastian.Kirsch@gmail.com>
2013-06-27 18:08:44 +0200

    dummy version

---
+++ src/main/java/de/is24/deadcode/plugin/FindDeadCodeMojo.java
@@ -0,0 +1,20 @@
+package de.is24.deadcode.plugin;
+
+import org.apache.maven.plugin.AbstractMojo;
+import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugin.MojoFailureException;
+
+
+/**
+ * The FindDeadCodeMojo attempts to find unused code.
+ *
+ * @goal find
+ * @execute phase="compile"
+ * @threadSafe true
+ */
+public class FindDeadCodeMojo extends AbstractMojo {
+  public void execute() throws MojoExecutionException, MojoFailureException {
+    getLog().warn("I'm not implemented yet!");
+  }
+
+}
