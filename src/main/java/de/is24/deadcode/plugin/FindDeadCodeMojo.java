81a31b15db610ffd85bfd47011ba5ff63e1ec3e0
sebastiankirsch <Sebastian.Kirsch@gmail.com>
2013-09-21 15:54:26 +0200

    description added

--- src/main/java/de/is24/deadcode/plugin/FindDeadCodeMojo.java
+++ src/main/java/de/is24/deadcode/plugin/FindDeadCodeMojo.java
@@ -15,6 +15,7 @@ import java.io.File;
 /**
  * The FindDeadCodeMojo attempts to find unused code.
  *
+ * @description Finds dead (unused) code
  * @goal find
  * @execute phase="compile"
  * @threadSafe true
