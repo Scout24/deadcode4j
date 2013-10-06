96d77d51b4a9e5fa59ad45d509e52a0bf8b2066f
sebastiankirsch <Sebastian.Kirsch@gmail.com>
2013-10-06 13:33:44 +0200

    refactoring XML analyzers: created/moved basic functionality tests

--- src/test/java/de/is24/deadcode4j/analyzer/A_SpringXmlAnalyzer.java
+++ src/test/java/de/is24/deadcode4j/analyzer/A_SpringXmlAnalyzer.java
@@ -1,14 +1,15 @@
 package de.is24.deadcode4j.analyzer;
 
-import com.google.common.collect.Iterables;
 import de.is24.deadcode4j.CodeContext;
 import javassist.ClassPool;
 import org.junit.Test;
 
 import java.util.Map;
 
+import static com.google.common.collect.Iterables.getOnlyElement;
 import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.*;
+import static org.hamcrest.Matchers.contains;
+import static org.hamcrest.Matchers.is;
 import static org.mockito.Mockito.mock;
 
 public final class A_SpringXmlAnalyzer {
@@ -22,30 +23,7 @@ public final class A_SpringXmlAnalyzer {
 
         Map<String, ? extends Iterable<String>> codeDependencies = codeContext.getAnalyzedCode().getCodeDependencies();
         assertThat("Should have analyzed the XML file!", codeDependencies.size(), is(1));
-        assertThat(Iterables.getOnlyElement(codeDependencies.values()), contains("SpringXmlBean"));
-    }
-
-    @Test
-    public void shouldParseSpringFilesWithNamespacePrefix() {
-        SpringXmlAnalyzer objectUnderTest = new SpringXmlAnalyzer();
-
-        CodeContext codeContext = new CodeContext(getClass().getClassLoader(), mock(ClassPool.class));
-        objectUnderTest.doAnalysis(codeContext, "spring-with-prefix.xml");
-
-        Map<String, ? extends Iterable<String>> codeDependencies = codeContext.getAnalyzedCode().getCodeDependencies();
-        assertThat("Should have analyzed the XML file!", codeDependencies.size(), is(1));
-        assertThat(Iterables.getOnlyElement(codeDependencies.values()), contains("SpringXmlBean"));
-    }
-
-    @Test
-    public void shouldNotParseNonSpringFiles() {
-        SpringXmlAnalyzer objectUnderTest = new SpringXmlAnalyzer();
-
-        CodeContext codeContext = new CodeContext(getClass().getClassLoader(), mock(ClassPool.class));
-        objectUnderTest.doAnalysis(codeContext, "nospring.xml");
-
-        assertThat("Should not have analyzed the XML file!", codeContext.getAnalyzedCode().getAnalyzedClasses(), hasSize(0));
-        assertThat("Should not have analyzed the XML file!", codeContext.getAnalyzedCode().getCodeDependencies().isEmpty());
+        assertThat(getOnlyElement(codeDependencies.values()), contains("SpringXmlBean"));
     }
 
 }
---
+++ src/test/java/de/is24/deadcode4j/analyzer/An_XmlAnalyzer.java
@@ -0,0 +1,160 @@
+package de.is24.deadcode4j.analyzer;
+
+import de.is24.deadcode4j.CodeContext;
+import javassist.ClassPool;
+import org.junit.Test;
+
+import java.util.Map;
+
+import static com.google.common.collect.Iterables.getOnlyElement;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
+import static org.mockito.Mockito.mock;
+
+public final class An_XmlAnalyzer {
+
+    @Test
+    public void parsesTheSpecifiedFile() {
+        XmlAnalyzer objectUnderTest = new XmlAnalyzer("junit", ".xml", null) {
+        };
+
+        CodeContext codeContext = createCodeContext();
+        objectUnderTest.doAnalysis(codeContext, "de/is24/deadcode4j/analyzer/empty.xml");
+
+        Map<String, ? extends Iterable<String>> codeDependencies = codeContext.getAnalyzedCode().getCodeDependencies();
+        assertThat("Should have analyzed the XML file!", codeDependencies.size(), is(1));
+    }
+
+    @Test
+    public void usesTheSpecifiedDependerIdToReportDependencies() {
+        final String dependerId = "junit";
+        XmlAnalyzer objectUnderTest = new XmlAnalyzer(dependerId, ".xml", null) {
+        };
+
+        CodeContext codeContext = createCodeContext();
+        objectUnderTest.doAnalysis(codeContext, "de/is24/deadcode4j/analyzer/empty.xml");
+
+        Map<String, ? extends Iterable<String>> codeDependencies = codeContext.getAnalyzedCode().getCodeDependencies();
+        assertThat("Should have analyzed the XML file!", codeDependencies.size(), is(1));
+        assertThat(codeDependencies.keySet(), contains(dependerId));
+    }
+
+    @Test
+    public void ignoresFilesWithNonMatchingEnding() {
+        XmlAnalyzer objectUnderTest = new XmlAnalyzer("junit", ".acme", null) {
+        };
+
+        CodeContext codeContext = createCodeContext();
+        objectUnderTest.doAnalysis(codeContext, "de/is24/deadcode4j/analyzer/empty.xml");
+
+        Map<String, ? extends Iterable<String>> codeDependencies = codeContext.getAnalyzedCode().getCodeDependencies();
+        assertThat("Should NOT have analyzed the XML file!", codeDependencies.size(), is(0));
+    }
+
+    @Test
+    public void ignoresFilesWithNonMatchingRootElement() {
+        XmlAnalyzer objectUnderTest = new XmlAnalyzer("junit", ".xml", "acme") {
+        };
+
+        CodeContext codeContext = createCodeContext();
+        objectUnderTest.doAnalysis(codeContext, "de/is24/deadcode4j/analyzer/empty.xml");
+
+        Map<String, ? extends Iterable<String>> codeDependencies = codeContext.getAnalyzedCode().getCodeDependencies();
+        assertThat("Should NOT have analyzed the XML file!", codeDependencies.size(), is(0));
+    }
+
+    @Test
+    public void reportsTheClassFoundForTheRegisteredElement() {
+        XmlAnalyzer objectUnderTest = new XmlAnalyzer("junit", ".xml", null) {
+            {
+                registerClassElement("elementWithClass");
+            }
+        };
+
+        CodeContext codeContext = createCodeContext();
+        objectUnderTest.doAnalysis(codeContext, "de/is24/deadcode4j/analyzer/some.xml");
+
+        Map<String, ? extends Iterable<String>> codeDependencies = codeContext.getAnalyzedCode().getCodeDependencies();
+        assertThat("Should have analyzed the XML file!", codeDependencies.size(), is(1));
+        assertThat(getOnlyElement(codeDependencies.values()), contains("de.is24.deadcode4j.ClassInElement"));
+    }
+
+    @Test
+    public void reportsTheClassFoundForTheRegisteredAttribute() {
+        XmlAnalyzer objectUnderTest = new XmlAnalyzer("junit", ".xml", null) {
+            {
+                registerClassAttribute("element", "attributeWithClass");
+            }
+        };
+
+        CodeContext codeContext = createCodeContext();
+        objectUnderTest.doAnalysis(codeContext, "de/is24/deadcode4j/analyzer/some.xml");
+
+        Map<String, ? extends Iterable<String>> codeDependencies = codeContext.getAnalyzedCode().getCodeDependencies();
+        assertThat("Should have analyzed the XML file!", codeDependencies.size(), is(1));
+        assertThat(getOnlyElement(codeDependencies.values()), contains("de.is24.deadcode4j.ClassInAttribute"));
+    }
+
+    @Test
+    public void reportsMultipleFoundClasses() {
+        XmlAnalyzer objectUnderTest = new XmlAnalyzer("junit", ".xml", null) {
+            {
+                registerClassElement("anotherElementWithClass");
+            }
+        };
+
+        CodeContext codeContext = createCodeContext();
+        objectUnderTest.doAnalysis(codeContext, "de/is24/deadcode4j/analyzer/some.xml");
+
+        Map<String, ? extends Iterable<String>> codeDependencies = codeContext.getAnalyzedCode().getCodeDependencies();
+        assertThat("Should have analyzed the XML file!", codeDependencies.size(), is(1));
+        assertThat(getOnlyElement(codeDependencies.values()),
+                containsInAnyOrder("de.is24.deadcode4j.FirstClassInAnotherElement",
+                        "de.is24.deadcode4j.SecondClassInAnotherElement"));
+    }
+
+    @Test
+    public void reportsClassesForDifferentRegistrations() {
+        XmlAnalyzer objectUnderTest = new XmlAnalyzer("junit", ".xml", null) {
+            {
+                registerClassElement("elementWithClass");
+                registerClassElement("anotherElementWithClass");
+                registerClassAttribute("element", "attributeWithClass");
+            }
+        };
+
+        CodeContext codeContext = createCodeContext();
+        objectUnderTest.doAnalysis(codeContext, "de/is24/deadcode4j/analyzer/some.xml");
+
+        Map<String, ? extends Iterable<String>> codeDependencies = codeContext.getAnalyzedCode().getCodeDependencies();
+        assertThat("Should have analyzed the XML file!", codeDependencies.size(), is(1));
+        assertThat(getOnlyElement(codeDependencies.values()),
+                containsInAnyOrder("de.is24.deadcode4j.ClassInElement", "de.is24.deadcode4j.FirstClassInAnotherElement",
+                        "de.is24.deadcode4j.SecondClassInAnotherElement", "de.is24.deadcode4j.ClassInAttribute"));
+    }
+
+    @Test
+    public void parsesXmlFilesUsingNamespacePrefixes() {
+        XmlAnalyzer objectUnderTest = new XmlAnalyzer("junit", ".xml", "root") {
+            {
+                registerClassElement("elementWithClass");
+                registerClassElement("anotherElementWithClass");
+                registerClassAttribute("element", "attributeWithClass");
+            }
+        };
+
+        CodeContext codeContext = createCodeContext();
+        objectUnderTest.doAnalysis(codeContext, "de/is24/deadcode4j/analyzer/prefixed.xml");
+
+        Map<String, ? extends Iterable<String>> codeDependencies = codeContext.getAnalyzedCode().getCodeDependencies();
+        assertThat("Should have analyzed the XML file!", codeDependencies.size(), is(1));
+        assertThat(getOnlyElement(codeDependencies.values()),
+                containsInAnyOrder("de.is24.deadcode4j.ClassInElement", "de.is24.deadcode4j.FirstClassInAnotherElement",
+                        "de.is24.deadcode4j.SecondClassInAnotherElement", "de.is24.deadcode4j.ClassInAttribute"));
+    }
+
+    private CodeContext createCodeContext() {
+        return new CodeContext(getClass().getClassLoader(), mock(ClassPool.class));
+    }
+
+}
