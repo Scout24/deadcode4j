cca84795b746e90f03718a22b71d1f384a69b2c6
sebastiankirsch <Sebastian.Kirsch@gmail.com>
2013-10-07 03:22:35 +0200

    fix test

--- src/test/java/de/is24/deadcode4j/plugin/IT_PuttingItAllTogether.java
+++ src/test/java/de/is24/deadcode4j/plugin/IT_PuttingItAllTogether.java
@@ -1,9 +1,12 @@
 package de.is24.deadcode4j.plugin;
 
+import org.apache.maven.plugin.MojoExecution;
 import org.apache.maven.plugin.logging.Log;
 import org.apache.maven.plugin.testing.AbstractMojoTestCase;
+import org.codehaus.plexus.util.ReflectionUtils;
 
 import java.io.File;
+import java.lang.reflect.Field;
 
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
@@ -25,6 +28,9 @@ public class IT_PuttingItAllTogether extends AbstractMojoTestCase {
 
         logMock = mock(Log.class);
         findDeadCodeMojo.setLog(logMock);
+        Field mojoExecution = ReflectionUtils.getFieldByNameIncludingSuperclasses("mojoExecution", FindDeadCodeMojo.class);
+        mojoExecution.setAccessible(true);
+        mojoExecution.set(findDeadCodeMojo, mock(MojoExecution.class));
     }
 
     public void test() throws Exception {
