b7b013e268e7725d4e5368caca95a686efba46b8
sebastiankirsch <Sebastian.Kirsch@gmail.com>
2013-11-08 14:49:17 +0100

    add analyzer for classes generated by castor (and probably accessed via reflection)

---
+++ src/main/java/de/is24/deadcode4j/analyzer/CastorClassesAnalyzer.java
@@ -0,0 +1,15 @@
+package de.is24.deadcode4j.analyzer;
+
+/**
+ * Analyzes class files: marks a class as being in use if it <i>directly</i> extends
+ * <code>org.exolab.castor.xml.util.XMLClassDescriptorImpl</code>.
+ *
+ * @since 1.4
+ */
+public class CastorClassesAnalyzer extends SuperClassAnalyzer {
+
+    public CastorClassesAnalyzer() {
+        super("_Castor-GeneratedClass_", "org.exolab.castor.xml.util.XMLClassDescriptorImpl");
+    }
+
+}
