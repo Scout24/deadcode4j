b2f27ef332500b93c67a8a8ea94668575abc35d6
sebastiankirsch <Sebastian.Kirsch@gmail.com>
2013-11-08 15:20:25 +0100

    added possibility to configure classes marking their direct descendants as live code

---
+++ src/main/java/de/is24/deadcode4j/analyzer/CustomSuperClassAnalyzer.java
@@ -0,0 +1,22 @@
+package de.is24.deadcode4j.analyzer;
+
+import javax.annotation.Nonnull;
+
+/**
+ * Analyzes class files: marks a class as being in use if it is a direct subclass of one of the specified classes.
+ *
+ * @since 1.4
+ */
+public class CustomSuperClassAnalyzer extends SuperClassAnalyzer {
+
+    /**
+     * Creates a new <code>CustomAnnotationsAnalyzer</code>.
+     *
+     * @param customSuperClasses a list of fully qualified class names indicating that the extending class is in use
+     * @since 1.4
+     */
+    public CustomSuperClassAnalyzer(@Nonnull Iterable<String> customSuperClasses) {
+        super("_custom-superclass_", customSuperClasses);
+    }
+
+}
