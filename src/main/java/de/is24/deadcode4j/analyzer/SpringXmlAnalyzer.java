c4ccde20ce2d8ca0d51be8059016a804dbea50d8
sebastiankirsch <Sebastian.Kirsch@gmail.com>
2013-11-21 22:27:14 +0100

    recognize view classes

--- src/main/java/de/is24/deadcode4j/analyzer/SpringXmlAnalyzer.java
+++ src/main/java/de/is24/deadcode4j/analyzer/SpringXmlAnalyzer.java
@@ -5,12 +5,15 @@ import de.is24.deadcode4j.Analyzer;
 /**
  * Analyzes Spring XML files:
  * <ul>
- *     <li>lists the <code>bean</code> classes being referenced</li>
- *     <li>lists the <a href="http://cxf.apache.org/schemas/jaxws.xsd">CXF <code>endpoint</code></a> implementor classes
- *     being referenced</li>
- *     <li>lists the classes executed by
- *     <a href="http://docs.spring.io/spring/docs/3.0.x/reference/scheduling.html#scheduling-quartz-jobdetail">Quartz
- *     jobs</a></li>
+ * <li>lists the <code>bean</code> classes being referenced</li>
+ * <li>lists the <a href="http://cxf.apache.org/schemas/jaxws.xsd">CXF <code>endpoint</code></a> implementor classes
+ * being referenced</li>
+ * <li>lists the classes executed by
+ * <a href="http://docs.spring.io/spring/docs/3.0.x/reference/scheduling.html#scheduling-quartz-jobdetail">Quartz
+ * jobs</a></li>
+ * <li>lists the view class used by
+ * <a href="http://docs.spring.io/spring/docs/3.0.x/reference/view.html#view-tiles-url">
+ * <code>UrlBasedViewResolver</code></a> and subclasses</li>
  * </ul>
  *
  * @since 1.1.0
@@ -23,6 +26,7 @@ public class SpringXmlAnalyzer extends SimpleXmlAnalyzer implements Analyzer {
         registerClassAttribute("endpoint", "implementor");
         registerClassAttribute("endpoint", "implementorClass");
         registerClassAttribute("property", "value").withAttributeValue("name", "jobClass");
+        registerClassAttribute("property", "value").withAttributeValue("name", "viewClass");
     }
 
 }
