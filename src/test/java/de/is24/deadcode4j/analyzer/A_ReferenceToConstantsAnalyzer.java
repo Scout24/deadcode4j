5930750b8bf4f7d479ae97263b07f8099573a9e7
sebastiankirsch <Sebastian.Kirsch@gmail.com>
2014-04-16 12:02:07 +0200

    enable other FQ tests

--- src/test/java/de/is24/deadcode4j/analyzer/A_ReferenceToConstantsAnalyzer.java
+++ src/test/java/de/is24/deadcode4j/analyzer/A_ReferenceToConstantsAnalyzer.java
@@ -13,36 +13,64 @@ import static org.hamcrest.Matchers.containsInAnyOrder;
 
 public final class A_ReferenceToConstantsAnalyzer extends AnAnalyzer {
 
+    private Analyzer objectUnderTest;
     private CodeContext codeContext;
 
     @Before
     public void setUp() throws Exception {
+        objectUnderTest = new ReferenceToConstantsAnalyzer();
         codeContext = new CodeContext();
     }
 
     @Test
     public void recognizesDependencyToConstantInMethod() {
-        Analyzer objectUnderTest = new ReferenceToConstantsAnalyzer();
-
-        CodeContext codeContext = new CodeContext();
         objectUnderTest.doAnalysis(codeContext, getFile("../../src/test/java/de/is24/deadcode4j/analyzer/constants/ClassUsingConstantInMethod.java"));
     }
 
     @Test
     public void recognizesDependencyToConstantInField() {
-        Analyzer objectUnderTest = new ReferenceToConstantsAnalyzer();
-
-        CodeContext codeContext = new CodeContext();
         objectUnderTest.doAnalysis(codeContext, getFile("../../src/test/java/de/is24/deadcode4j/analyzer/constants/ClassUsingConstantInField.java"));
     }
 
     @Test
     public void recognizesDependencyToFullyQualifiedConstantInField() {
-        Analyzer objectUnderTest = new ReferenceToConstantsAnalyzer();
-
         objectUnderTest.doAnalysis(codeContext, getFile("../../src/test/java/de/is24/deadcode4j/analyzer/constants/ClassUsingFQConstantInField.java"));
 
         assertDependencyExists("de.is24.deadcode4j.analyzer.constants.ClassUsingFQConstantInField", "de.is24.deadcode4j.analyzer.constants.Constants");
+    }
+
+    @Test
+    public void recognizesDependencyToConstantOfOtherPackageInMethod() {
+        objectUnderTest.doAnalysis(codeContext, getFile("../../src/test/java/de/is24/deadcode4j/analyzer/constants/subpackage/ClassUsingConstantOfOtherPackageInMethod.java"));
+    }
+
+    @Test
+    public void recognizesDependencyToFullyQualifiedConstantOfOtherPackageInMethod() {
+        objectUnderTest.doAnalysis(codeContext, getFile("../../src/test/java/de/is24/deadcode4j/analyzer/constants/subpackage/ClassUsingFQConstantOfOtherPackageInMethod.java"));
+
+        assertDependencyExists("de.is24.deadcode4j.analyzer.constants.subpackage.ClassUsingFQConstantOfOtherPackageInMethod", "de.is24.deadcode4j.analyzer.constants.Constants");
+    }
+
+    @Test
+    public void recognizesDependencyToConstantOfOtherPackageReferencedViaStaticImportInMethod() {
+        objectUnderTest.doAnalysis(codeContext, getFile("../../src/test/java/de/is24/deadcode4j/analyzer/constants/subpackage/ClassUsingStaticImportForConstantOfOtherPackageInMethod.java"));
+    }
+
+    @Test
+    public void recognizesOverwrittenConstantOfOtherPackageReferencedViaStaticImportInMethod() {
+        objectUnderTest.doAnalysis(codeContext, getFile("../../src/test/java/de/is24/deadcode4j/analyzer/constants/ClassUsingStaticImportForConstantWithSameLocalName.java"));
+    }
+
+    @Test
+    public void recognizesDependencyToFullyQualifiedConstantInExpression() {
+        objectUnderTest.doAnalysis(codeContext, getFile("../../src/test/java/de/is24/deadcode4j/analyzer/constants/ClassUsingFQConstantInExpression.java"));
+
+        assertDependencyExists("de.is24.deadcode4j.analyzer.constants.ClassUsingFQConstantInExpression", "de.is24.deadcode4j.analyzer.constants.Constants");
+    }
+
+    @Test
+    public void recognizesDependencyToInnerClassInsteadOfPackageClass() {
+        objectUnderTest.doAnalysis(codeContext, getFile("../../src/test/java/de/is24/deadcode4j/analyzer/constants/ClassWithInnerClassNamedLikePotentialTarget.java"));
     }
 
     private void assertDependencyExists(String depender, String dependee) {
@@ -51,54 +79,6 @@ public final class A_ReferenceToConstantsAnalyzer extends AnAnalyzer {
 
         Iterable<String> allReportedClasses = concat(codeDependencies.values());
         assertThat(allReportedClasses, containsInAnyOrder(dependee));
-    }
-
-    @Test
-    public void recognizesDependencyToConstantOfOtherPackageInMethod() {
-        Analyzer objectUnderTest = new ReferenceToConstantsAnalyzer();
-
-        CodeContext codeContext = new CodeContext();
-        objectUnderTest.doAnalysis(codeContext, getFile("../../src/test/java/de/is24/deadcode4j/analyzer/constants/subpackage/ClassUsingConstantOfOtherPackageInMethod.java"));
-    }
-
-    @Test
-    public void recognizesDependencyToFullyQualifiedConstantOfOtherPackageInMethod() {
-        Analyzer objectUnderTest = new ReferenceToConstantsAnalyzer();
-
-        CodeContext codeContext = new CodeContext();
-        objectUnderTest.doAnalysis(codeContext, getFile("../../src/test/java/de/is24/deadcode4j/analyzer/constants/subpackage/ClassUsingFQConstantOfOtherPackageInMethod.java"));
-    }
-
-    @Test
-    public void recognizesDependencyToConstantOfOtherPackageReferencedViaStaticImportInMethod() {
-        Analyzer objectUnderTest = new ReferenceToConstantsAnalyzer();
-
-        CodeContext codeContext = new CodeContext();
-        objectUnderTest.doAnalysis(codeContext, getFile("../../src/test/java/de/is24/deadcode4j/analyzer/constants/subpackage/ClassUsingStaticImportForConstantOfOtherPackageInMethod.java"));
-    }
-
-    @Test
-    public void recognizesOverwrittenConstantOfOtherPackageReferencedViaStaticImportInMethod() {
-        Analyzer objectUnderTest = new ReferenceToConstantsAnalyzer();
-
-        CodeContext codeContext = new CodeContext();
-        objectUnderTest.doAnalysis(codeContext, getFile("../../src/test/java/de/is24/deadcode4j/analyzer/constants/ClassUsingStaticImportForConstantWithSameLocalName.java"));
-    }
-
-    @Test
-    public void recognizesDependencyToConstantReferencedViaStaticImportInExpression() {
-        Analyzer objectUnderTest = new ReferenceToConstantsAnalyzer();
-
-        CodeContext codeContext = new CodeContext();
-        objectUnderTest.doAnalysis(codeContext, getFile("../../src/test/java/de/is24/deadcode4j/analyzer/constants/ClassUsingFQConstantInExpression.java"));
-    }
-
-    @Test
-    public void recognizesDependencyToInnerClassInsteadOfPackageClass() {
-        Analyzer objectUnderTest = new ReferenceToConstantsAnalyzer();
-
-        CodeContext codeContext = new CodeContext();
-        objectUnderTest.doAnalysis(codeContext, getFile("../../src/test/java/de/is24/deadcode4j/analyzer/constants/ClassWithInnerClassNamedLikePotentialTarget.java"));
     }
 
 }
