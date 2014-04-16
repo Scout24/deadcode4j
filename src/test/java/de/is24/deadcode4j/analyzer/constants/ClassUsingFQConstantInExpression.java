bd8df22d42aca2b764ac39ba7706be13858f91fa
sebastiankirsch <Sebastian.Kirsch@gmail.com>
2014-04-16 11:54:40 +0200

    do what name suggests

--- src/test/java/de/is24/deadcode4j/analyzer/constants/ClassUsingFQConstantInExpression.java
+++ src/test/java/de/is24/deadcode4j/analyzer/constants/ClassUsingFQConstantInExpression.java
@@ -1,11 +1,8 @@
 package de.is24.deadcode4j.analyzer.constants;
-
-import static de.is24.deadcode4j.analyzer.constants.Constants.FOO;
-
 @SuppressWarnings("UnusedDeclaration")
 public class ClassUsingFQConstantInExpression {
     @Override
     public String toString() {
-        return String.valueOf( "har".equals(FOO));
+        return String.valueOf( "har".equals(de.is24.deadcode4j.analyzer.constants.Constants.FOO));
     }
 }
