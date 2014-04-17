89dd6d835ed1b600d3995c779fc30ccc9ef2c387
sebastiankirsch <Sebastian.Kirsch@gmail.com>
2014-04-17 17:35:57 +0200

    extracted the file loading logic

--- src/test/java/de/is24/deadcode4j/analyzer/AnAnalyzer.java
+++ src/test/java/de/is24/deadcode4j/analyzer/AnAnalyzer.java
@@ -1,5 +1,6 @@
 package de.is24.deadcode4j.analyzer;
 
+import de.is24.deadcode4j.junit.FileLoader;
 import de.is24.deadcode4j.junit.LoggingRule;
 import org.junit.Rule;
 
@@ -11,11 +12,7 @@ public abstract class AnAnalyzer {
     public final LoggingRule enableLogging = new LoggingRule();
 
     protected File getFile(String fileName) {
-        String classFile = getClass().getSimpleName() + ".class";
-        String pathToClass = getClass().getResource(classFile).getFile();
-        String baseDir = pathToClass.substring(0, pathToClass.length() -
-                (getClass().getPackage().getName() + "/" + classFile).length());
-        return new File(new File(baseDir), fileName);
+        return FileLoader.getFile(fileName);
     }
 
 }
