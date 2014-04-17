22b109e0ff25ba3e97423a9412e9bd9f985911ab
sebastiankirsch <Sebastian.Kirsch@gmail.com>
2014-04-17 23:09:07 +0200

    dang that auto-import

--- src/test/java/de/is24/deadcode4j/analyzer/constants/subpackage/ClassUsingConstantOfOtherPackageViaAsteriskImportInMethod.java
+++ src/test/java/de/is24/deadcode4j/analyzer/constants/subpackage/ClassUsingConstantOfOtherPackageViaAsteriskImportInMethod.java
@@ -1,7 +1,5 @@
 package de.is24.deadcode4j.analyzer.constants.subpackage;
-
-import de.is24.deadcode4j.analyzer.constants.Constants;
-
+import de.is24.deadcode4j.analyzer.constants.*;
 @SuppressWarnings("UnusedDeclaration")
 public class ClassUsingConstantOfOtherPackageViaAsteriskImportInMethod {
     @Override
