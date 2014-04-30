08a02392f859b2f1d72eb2389ecc9e852967cf01
sebastiankirsch <Sebastian.Kirsch@gmail.com>
2014-04-30 15:01:32 +0200

    speed up performance by checking the field access from the smallest unit to the bigger ones

--- src/main/java/de/is24/deadcode4j/analyzer/ReferenceToConstantsAnalyzer.java
+++ src/main/java/de/is24/deadcode4j/analyzer/ReferenceToConstantsAnalyzer.java
@@ -407,16 +407,18 @@ public class ReferenceToConstantsAnalyzer extends AnalyzerAdapter {
         }
 
         private boolean refersToClass(@Nonnull FieldAccessExpr fieldAccessExpr, @Nonnull Analysis analysis, @Nonnull String qualifierPrefix) {
-            String resolvedClass = resolveClass(qualifierPrefix + fieldAccessExpr.toString());
-            if (resolvedClass != null) {
-                codeContext.addDependencies(analysis.getTypeName(), resolvedClass);
+            if (NameExpr.class.isInstance(fieldAccessExpr.getScope())) {
+                String resolvedClass = resolveClass(qualifierPrefix + NameExpr.class.cast(fieldAccessExpr.getScope()).getName());
+                if (resolvedClass != null) {
+                    codeContext.addDependencies(analysis.getTypeName(), resolvedClass);
+                    return true;
+                }
+                return false;
+            }
+            if (refersToClass(FieldAccessExpr.class.cast(fieldAccessExpr.getScope()), analysis, qualifierPrefix)) {
                 return true;
             }
-            if (FieldAccessExpr.class.isInstance(fieldAccessExpr.getScope())) {
-                return refersToClass(FieldAccessExpr.class.cast(fieldAccessExpr.getScope()), analysis, qualifierPrefix);
-            }
-
-            resolvedClass = resolveClass(qualifierPrefix + NameExpr.class.cast(fieldAccessExpr.getScope()).getName());
+            String resolvedClass = resolveClass(qualifierPrefix + fieldAccessExpr.toString());
             if (resolvedClass != null) {
                 codeContext.addDependencies(analysis.getTypeName(), resolvedClass);
                 return true;
