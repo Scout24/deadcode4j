397475b4277244441e620f898110d2a2b82a631c
sebastiankirsch <Sebastian.Kirsch@gmail.com>
2014-05-02 20:25:48 +0200

    use JDK6 class

--- src/test/java/de/is24/deadcode4j/analyzer/typeerasure/TypedArrayList.java
+++ src/test/java/de/is24/deadcode4j/analyzer/typeerasure/TypedArrayList.java
@@ -3,10 +3,10 @@ import java.util.ArrayList;
 import java.util.Map;
 import java.util.concurrent.*;
 import java.util.regex.*;
-import static java.util.Locale.Category;
+import static java.util.ResourceBundle.Control;
 @SuppressWarnings("UnusedDeclaration")
 public class TypedArrayList extends ArrayList<Map.Entry<java.math.BigDecimal, Comparable>> {
-    private Map<Pattern, Category> someMap;
+    private Map<Pattern, Control> someMap;
     @Override
     public String toString() {
         return new ArrayList<InnerClass>().toString();
