6e13b6c3875bdabd44580b515a1b77caab9dc4f9
sebastiankirsch <Sebastian.Kirsch@gmail.com>
2014-05-07 13:52:07 +0200

    committed local BS

--- src/test/java/de/is24/deadcode4j/analyzer/typeerasure/ClassWithUpperBoundedWildCard.java
+++ src/test/java/de/is24/deadcode4j/analyzer/typeerasure/ClassWithUpperBoundedWildCard.java
@@ -1,6 +1,7 @@
 package de.is24.deadcode4j.analyzer.typeerasure;
 import java.util.ArrayList;
+import java.util.Collection;
 @SuppressWarnings("UnusedDeclaration")
 public class ClassWithUpperBoundedWildCard {
-    private ArrayList<long[]> aList;
+    private ArrayList<Collection> aList;
 }
