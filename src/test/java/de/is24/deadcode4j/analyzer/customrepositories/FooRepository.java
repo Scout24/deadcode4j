2be12aaf06bfc7ad7239a2bdce9a1d935dcbb8f3
sebastiankirsch <Sebastian.Kirsch@gmail.com>
2014-05-15 17:00:03 +0200

    set up spring data repo correctly

--- src/test/java/de/is24/deadcode4j/analyzer/customrepositories/FooRepository.java
+++ src/test/java/de/is24/deadcode4j/analyzer/customrepositories/FooRepository.java
@@ -1,3 +1,4 @@
 package de.is24.deadcode4j.analyzer.customrepositories;
+import org.springframework.data.repository.Repository;
 @SuppressWarnings("UnusedDeclaration")
-public interface FooRepository { }
+public interface FooRepository extends Repository<Foo, Long>, FooRepositoryCustom { }
