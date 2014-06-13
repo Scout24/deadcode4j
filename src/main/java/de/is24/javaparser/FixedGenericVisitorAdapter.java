74788fa4c780dffd6d48f7825d56beee016c2b14
sebastiankirsch <Sebastian.Kirsch@gmail.com>
2014-06-13 16:45:34 +0200

    handle another set of bugs in javaparser

--- src/main/java/de/is24/javaparser/FixedGenericVisitorAdapter.java
+++ src/main/java/de/is24/javaparser/FixedGenericVisitorAdapter.java
@@ -1,12 +1,22 @@
 package de.is24.javaparser;
 
+import de.is24.deadcode4j.Utils;
 import japa.parser.ast.body.AnnotationMemberDeclaration;
 import japa.parser.ast.expr.Expression;
+import japa.parser.ast.expr.VariableDeclarationExpr;
+import japa.parser.ast.stmt.TryStmt;
 import japa.parser.ast.visitor.GenericVisitorAdapter;
 
 /**
- * Fixes a bug in {@link japa.parser.ast.body.AnnotationMemberDeclaration} where the default value expression isn't set
- * as a child of the declaration.
+ * Fixes several bugs:
+ * <ul>
+ *     <li>in {@link japa.parser.ast.body.AnnotationMemberDeclaration}, the default value expression isn't set as a
+ *     child of the declaration</li>
+ *     <li>in {@link japa.parser.ast.stmt.TryStmt}, the variable declarations aren't set as a child of the try
+ *     statement</li>
+ *     <li>in {@link japa.parser.ast.visitor.GenericVisitorAdapter#visit(japa.parser.ast.stmt.TryStmt, Object)},
+ *     the variable declarations aren't visited</li>
+ * </ul>
  *
  * @since 1.6
  */
@@ -17,6 +27,18 @@ public class FixedGenericVisitorAdapter<R, A> extends GenericVisitorAdapter<R, A
         Expression defaultValue = n.getDefaultValue();
         if (defaultValue != null) {
             defaultValue.setParentNode(n);
+        }
+        return super.visit(n, arg);
+    }
+
+    @Override
+    public R visit(TryStmt n, A arg) {
+        for (VariableDeclarationExpr variableDeclarationExpr : Utils.emptyIfNull(n.getResources())) {
+            variableDeclarationExpr.setParentNode(n);
+            R result = variableDeclarationExpr.accept(this, arg);
+            if (result != null) {
+                return result;
+            }
         }
         return super.visit(n, arg);
     }
