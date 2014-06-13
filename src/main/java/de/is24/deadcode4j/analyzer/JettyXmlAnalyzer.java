5c07de747b84c0ee5f21a07127aba90a1c96eb66
sebastiankirsch <Sebastian.Kirsch@gmail.com>
2014-06-13 12:29:21 +0200

    analyze Jetty XML files

---
+++ src/main/java/de/is24/deadcode4j/analyzer/JettyXmlAnalyzer.java
@@ -0,0 +1,25 @@
+package de.is24.deadcode4j.analyzer;
+
+/**
+ * Analyzes <a href="http://www.eclipse.org/jetty/configure_9_0.dtd">Jetty XML configuration</a> files.
+ * Reports the {@code class} and {@code type} attributes as classes being referenced.
+ *
+ * @since 1.6
+ */
+public class JettyXmlAnalyzer extends SimpleXmlAnalyzer {
+
+    public JettyXmlAnalyzer() {
+        super("_Jetty-XML_", ".xml", "Configure");
+        registerClassAttribute("Arg", "type");
+        registerClassAttribute("Array", "type");
+        registerClassAttribute("Call", "class");
+        registerClassAttribute("Configure", "class");
+        registerClassAttribute("Get", "class");
+        registerClassAttribute("Item", "type");
+        registerClassAttribute("New", "class");
+        registerClassAttribute("Put", "type");
+        registerClassAttribute("Set", "class");
+        registerClassAttribute("Set", "type");
+    }
+
+}
