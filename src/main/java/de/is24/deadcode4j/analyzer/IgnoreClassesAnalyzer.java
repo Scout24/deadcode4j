3feb67b562799e70463983785de66fb9315501f2
sebastiankirsch <Sebastian.Kirsch@gmail.com>
2014-09-12 13:20:31 +0200

    clean code; documentation added

--- src/main/java/de/is24/deadcode4j/analyzer/IgnoreClassesAnalyzer.java
+++ src/main/java/de/is24/deadcode4j/analyzer/IgnoreClassesAnalyzer.java
@@ -9,12 +9,16 @@ import javassist.CtClass;
 
 import javax.annotation.Nonnull;
 import java.util.ArrayList;
-import java.util.Collection;
 import java.util.Set;
 
 import static com.google.common.collect.Sets.newHashSet;
 import static com.google.common.collect.Sets.newHashSetWithExpectedSize;
 
+/**
+ * Analyzes class files: marks a class as being in use if it should be ignored.
+ *
+ * @since 2.0.0
+ */
 public class IgnoreClassesAnalyzer extends ByteCodeAnalyzer {
 
     @Nonnull
@@ -38,27 +42,36 @@ public class IgnoreClassesAnalyzer extends ByteCodeAnalyzer {
 
     @Override
     public void finishAnalysis(@Nonnull AnalysisSink analysisSink, @Nonnull AnalyzedCode analyzedCode) {
+        logUnknownClassesThatShouldBeIgnored();
+        logLiveClassesThatShouldBeIgnored(analyzedCode);
+        logIgnoredClasses();
+
+        analysisSink.addDependencies("_IgnoredClasses_", this.ignoredClasses);
+    }
+
+    private void logUnknownClassesThatShouldBeIgnored() {
         ArrayList<String> ignoredButUnknownClasses = Lists.newArrayList(this.classesToIgnore);
         ignoredButUnknownClasses.removeAll(this.ignoredClasses);
         for (String ignoredButUnknownClass : ignoredButUnknownClasses) {
             logger.warn("Class [{}] should be ignored, but does not exist. You should remove the configuration entry.",
                     ignoredButUnknownClass);
         }
+    }
 
-        Collection<String> deadClasses = new DeadCodeComputer().computeDeadCode(analyzedCode).getDeadClasses();
+    private void logLiveClassesThatShouldBeIgnored(AnalyzedCode analyzedCode) {
         ArrayList<String> ignoredButExistingClasses = Lists.newArrayList(this.ignoredClasses);
-        ignoredButExistingClasses.removeAll(deadClasses);
+        ignoredButExistingClasses.removeAll(new DeadCodeComputer().computeDeadCode(analyzedCode).getDeadClasses());
         for (String ignoredButExistingClass : ignoredButExistingClasses) {
             logger.warn("Class [{}] should be ignored, but is not dead. You should remove the configuration entry.",
                     ignoredButExistingClass);
             this.ignoredClasses.remove(ignoredButExistingClass);
         }
+    }
 
+    private void logIgnoredClasses() {
         if (this.ignoredClasses.size() != 0) {
             logger.info("Ignoring {} class(es) which seem(s) to be unused.", this.ignoredClasses.size());
         }
-        analysisSink.addDependencies("_IgnoredClasses_",
-                this.ignoredClasses.toArray(new String[this.ignoredClasses.size()]));
     }
 
 }
