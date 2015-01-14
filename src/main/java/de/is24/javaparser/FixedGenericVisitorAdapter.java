bb4075e4e2d3179014d034568ff04e8b1db39171
sebastiankirsch <Sebastian.Kirsch@gmail.com>
2015-01-14 23:05:05 +0100

    remove custom fixes for javaparser 1.0.11

--- src/main/java/de/is24/javaparser/FixedGenericVisitorAdapter.java
+++ src/main/java/de/is24/javaparser/FixedGenericVisitorAdapter.java
@@ -4,23 +4,14 @@ import com.github.javaparser.ast.Node;
 import com.github.javaparser.ast.body.Parameter;
 import com.github.javaparser.ast.expr.AnnotationExpr;
 import com.github.javaparser.ast.expr.LambdaExpr;
-import de.is24.deadcode4j.Utils;
-import com.github.javaparser.ast.body.AnnotationMemberDeclaration;
-import com.github.javaparser.ast.expr.Expression;
-import com.github.javaparser.ast.expr.VariableDeclarationExpr;
-import com.github.javaparser.ast.stmt.TryStmt;
 import com.github.javaparser.ast.visitor.GenericVisitorAdapter;
 
 /**
  * Fixes several bugs:
  * <ul>
- *     <li>in {@link AnnotationMemberDeclaration}, the default value expression isn't set as a child of the
- *     declaration</li>
- *     <li>in {@link LambdaExpr}, the body and parameters aren't set as a child of the lambda expression</li>
- *     <li>in {@link TryStmt}, the variable declarations aren't set as a child of the try statement</li>
- *     <li>in {@link GenericVisitorAdapter#visit(Parameter, Object)}, the parameter's type may be <code>null</code> and
- *     thus cause a <code>NullPointerException</code></li>
- *     <li>in {@link GenericVisitorAdapter#visit(TryStmt, Object)}, the variable declarations aren't visited</li>
+ * <li>in {@link LambdaExpr}, the body and parameters aren't set as a child of the lambda expression</li>
+ * <li>in {@link GenericVisitorAdapter#visit(Parameter, Object)}, the parameter's type may be <code>null</code> and thus
+ * cause a <code>NullPointerException</code></li>
  * </ul>
  *
  * @since 2.0.0
@@ -37,18 +28,9 @@ public class FixedGenericVisitorAdapter<R, A> extends GenericVisitorAdapter<R, A
         if (childNodes == null) {
             return;
         }
-        for (Node childNode: childNodes) {
+        for (Node childNode : childNodes) {
             setAsParentNode(parentNode, childNode);
         }
-    }
-
-    @Override
-    public R visit(AnnotationMemberDeclaration n, A arg) {
-        Expression defaultValue = n.getDefaultValue();
-        if (defaultValue != null) {
-            defaultValue.setParentNode(n);
-        }
-        return super.visit(n, arg);
     }
 
     @Override
@@ -83,18 +65,6 @@ public class FixedGenericVisitorAdapter<R, A> extends GenericVisitorAdapter<R, A
             }
         }
         return null;
-    }
-
-    @Override
-    public R visit(TryStmt n, A arg) {
-        for (VariableDeclarationExpr variableDeclarationExpr : Utils.emptyIfNull(n.getResources())) {
-            variableDeclarationExpr.setParentNode(n);
-            R result = variableDeclarationExpr.accept(this, arg);
-            if (result != null) {
-                return result;
-            }
-        }
-        return super.visit(n, arg);
     }
 
 }
--- src/main/java/de/is24/javaparser/FixedVoidVisitorAdapter.java
+++ src/main/java/de/is24/javaparser/FixedVoidVisitorAdapter.java
@@ -1,26 +1,18 @@
 package de.is24.javaparser;
 
 import com.github.javaparser.ast.Node;
-import com.github.javaparser.ast.body.AnnotationMemberDeclaration;
 import com.github.javaparser.ast.body.Parameter;
 import com.github.javaparser.ast.comments.Comment;
 import com.github.javaparser.ast.expr.AnnotationExpr;
-import com.github.javaparser.ast.expr.Expression;
 import com.github.javaparser.ast.expr.LambdaExpr;
-import com.github.javaparser.ast.expr.VariableDeclarationExpr;
-import com.github.javaparser.ast.stmt.TryStmt;
 import com.github.javaparser.ast.visitor.VoidVisitorAdapter;
-import de.is24.deadcode4j.Utils;
 
 /**
  * Fixes several bugs:
  * <ul>
- * <li>in {@link AnnotationMemberDeclaration}, the default value expression isn't set as a child of the declaration</li>
  * <li>in {@link LambdaExpr}, the body and parameters aren't set as a child of the lambda expression</li>
- * <li>in {@link TryStmt}, the variable declarations aren't set as a child of the try statement</li>
  * <li>in {@link VoidVisitorAdapter#visit(Parameter, Object)}, the parameter's type may be <code>null</code> and thus
  * cause a <code>NullPointerException</code></li>
- * <li>in {@link VoidVisitorAdapter#visit(TryStmt, Object)}, the variable declarations aren't visited</li>
  * </ul>
  *
  * @since 2.0.0
@@ -43,18 +35,9 @@ public class FixedVoidVisitorAdapter<A> extends VoidVisitorAdapter<A> {
         if (childNodes == null) {
             return;
         }
-        for (Node childNode: childNodes) {
+        for (Node childNode : childNodes) {
             setAsParentNode(parentNode, childNode);
         }
-    }
-
-    @Override
-    public void visit(AnnotationMemberDeclaration n, A arg) {
-        Expression defaultValue = n.getDefaultValue();
-        if (defaultValue != null) {
-            defaultValue.setParentNode(n);
-        }
-        super.visit(n, arg);
     }
 
     @Override
@@ -76,15 +59,6 @@ public class FixedVoidVisitorAdapter<A> extends VoidVisitorAdapter<A> {
             n.getType().accept(this, arg);
         }
         n.getId().accept(this, arg);
-    }
-
-    @Override
-    public void visit(TryStmt n, A arg) {
-        for (VariableDeclarationExpr variableDeclarationExpr : Utils.emptyIfNull(n.getResources())) {
-            variableDeclarationExpr.setParentNode(n);
-            variableDeclarationExpr.accept(this, arg);
-        }
-        super.visit(n, arg);
     }
 
 }
