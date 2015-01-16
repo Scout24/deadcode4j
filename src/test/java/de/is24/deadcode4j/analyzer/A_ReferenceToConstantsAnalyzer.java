dafb31de3a0f119a2566c6430cdc09014da2f708
sebastiankirsch <Sebastian.Kirsch@gmail.com>
2015-01-16 11:38:00 +0100

    the method said: don't commit :/

--- src/test/java/de/is24/deadcode4j/analyzer/A_ReferenceToConstantsAnalyzer.java
+++ src/test/java/de/is24/deadcode4j/analyzer/A_ReferenceToConstantsAnalyzer.java
@@ -45,11 +45,6 @@ public final class A_ReferenceToConstantsAnalyzer extends AnAnalyzer<ReferenceTo
     }
 
     @Test
-    public void dontCommit() {
-        analyzeFile("../../../../is24/checkout/app/src/main/java/de/is24/checkout/controller/internal/LocalLoginController.java");
-    }
-
-    @Test
     public void recognizesDependencyToConstantInExpression() {
         analyzeFile("../../src/test/java/de/is24/deadcode4j/analyzer/constants/ClassUsingConstantInExpression.java");
         triggerFinishAnalysisEvent();
