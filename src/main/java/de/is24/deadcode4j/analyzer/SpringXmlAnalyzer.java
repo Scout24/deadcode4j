83a39a91afe2c580313a991c0bb198fd22806afc
sebastiankirsch <Sebastian.Kirsch@gmail.com>
2016-02-10 10:42:49 +0100

    support the JobDetailFactoryBean introduced with Spring 3.1

--- src/main/java/de/is24/deadcode4j/analyzer/SpringXmlAnalyzer.java
+++ src/main/java/de/is24/deadcode4j/analyzer/SpringXmlAnalyzer.java
@@ -11,9 +11,9 @@ import javax.annotation.Nonnull;
  * more verbose approach using the <tt>targetClass</tt> and <tt>targetMethod</tt> properties</li>
  * <li>lists the <a href="http://cxf.apache.org/schemas/jaxws.xsd">CXF <code>endpoint</code></a> implementor classes
  * being referenced</li>
- * <li>lists the classes executed by
- * <a href="http://docs.spring.io/spring/docs/3.0.x/reference/scheduling.html#scheduling-quartz-jobdetail">Quartz
- * jobs</a></li>
+ * <li>lists the classes executed by Quartz via
+ * <a href="http://docs.spring.io/spring/docs/3.0.x/reference/scheduling.html#scheduling-quartz-jobdetail">JobDetailBean</a></li> or
+ * <a href="http://docs.spring.io/spring/docs/4.0.x/spring-framework-reference/html/scheduling.html#scheduling-quartz-jobdetail">JobDetailFactoryBean</a>
  * <li>lists the view class used by
  * <a href="http://docs.spring.io/spring/docs/3.0.x/reference/view.html#view-tiles-url">
  * <code>UrlBasedViewResolver</code></a> and subclasses</li>
@@ -44,6 +44,10 @@ public class SpringXmlAnalyzer extends ExtendedXmlAnalyzer {
         // JobDetailBean
         registerPropertyValueAsClass(
                 beanOfClass("org.springframework.scheduling.quartz.JobDetailBean"),
+                "jobClass");
+        // JobDetailFactoryBean
+        registerPropertyValueAsClass(
+                beanOfClass("org.springframework.scheduling.quartz.JobDetailFactoryBean"),
                 "jobClass");
         // view resolver; this is not restricted to a class bc there are loads of subclasses
         registerPropertyValueAsClass(anyElementNamed("bean"), "viewClass");
