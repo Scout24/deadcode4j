5674824f2c9a69f29ef4395651018e57d5038997
sebastiankirsch <Sebastian.Kirsch@gmail.com>
2016-02-11 11:20:26 +0100

    unit test exception handling

--- src/test/java/de/is24/deadcode4j/analyzer/A_SpringNamespaceHandlerAnalyzer.java
+++ src/test/java/de/is24/deadcode4j/analyzer/A_SpringNamespaceHandlerAnalyzer.java
@@ -1,8 +1,20 @@
 package de.is24.deadcode4j.analyzer;
 
+import mockit.Mock;
+import mockit.MockUp;
 import org.junit.Test;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.containsString;
+import static org.junit.Assert.fail;
+
 public final class A_SpringNamespaceHandlerAnalyzer extends AnAnalyzer<SpringNamespaceHandlerAnalyzer> {
+
+    private static final String SPRING_HANDLER_FILE = "META-INF/spring.handlers";
 
     @Override
     protected SpringNamespaceHandlerAnalyzer createAnalyzer() {
@@ -11,9 +23,26 @@ public final class A_SpringNamespaceHandlerAnalyzer extends AnAnalyzer<SpringNam
 
     @Test
     public void shouldRecognizeDefinedNamespaceHandlers() {
-        analyzeFile("META-INF/spring.handlers");
+        analyzeFile(SPRING_HANDLER_FILE);
 
         assertThatDependenciesAreReported("CustomNamespaceHandler", "AnotherNamespaceHandler");
+    }
+
+    @Test
+    public void handlesException() throws Exception {
+        new MockUp<FileInputStream>() {
+            @Mock
+            public void $init(File file) throws IOException {
+                throw new IOException("JUnit");
+            }
+        };
+
+        try {
+            analyzeFile(SPRING_HANDLER_FILE);
+            fail("Should abort analysis!");
+        } catch (RuntimeException e) {
+            assertThat(e.getMessage(), containsString(SPRING_HANDLER_FILE));
+        }
     }
 
 }
