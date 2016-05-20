385beec7b9a6e0312782c901fb62fbe21654f194
sebastiankirsch <Sebastian.Kirsch@immobilienscout24.de>
2016-05-20 15:56:46 +0200

    ignore beta stuff

--- tools/update-versions.sh
+++ tools/update-versions.sh
@@ -1,3 +1,5 @@
 #!/usr/bin/env bash
 
-mvn versions:update-parent versions:update-properties versions:use-latest-releases
+excludes=javax.servlet:javax.servlet-api # we're using some kind of legacy version here
+
+mvn versions:update-parent versions:update-properties versions:use-latest-releases -Dexcludes=${excludes} -DgenerateBackupPoms=false -Dmaven.version.rules=file://\${project.basedir}/config/version-rules.xml
